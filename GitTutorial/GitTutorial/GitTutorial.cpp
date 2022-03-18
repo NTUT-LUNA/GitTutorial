e2b40e2fd9cbe537b554624de63a2e147d8269ca
NTUT_LUNA <t110aem002@ntut.org.tw>
2022-03-18 10:01:52 +0800

    updated main function

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 bool Guess(int number) {
 	return false;
 }
 
 int main() {
-
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
